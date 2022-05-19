869f530373b78a2428380dc071b87f17585dfbb6
bhs0906 <hg5112@naver.com>
2022-05-19 19:11:13 +0900

    ver2

---
+++ Model/CompanyService.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class CompanyService {
+
+    public CompanyService() {
+    }
+
+    public void saveWorker() {
+        // TODO implement here
+        return null;
+    }
+
+    public void modify() {
+        // TODO implement here
+        return null;
+    }
+
+    public void search() {
+        // TODO implement here
+        return null;
+    }
+
+    public void outputTitle() {
+        // TODO implement here
+        return null;
+    }
+
+    public void outpub() {
+        // TODO implement here
+        return null;
+    }
+
+}
